2cae0a8e30e75411fe062b4acf562c074739dc14
ankit lakhiwal <lakhiwalankit0002@gmail.com>
2022-11-09 18:47:09 +0000

    Update DataTable.tsx and IITKMap.tsx

--- components/IITKMap.tsx
+++ components/IITKMap.tsx
@@ -7,7 +7,7 @@ interface dataProps {
 
 export default function IITKMap({ data }: dataProps) {
   return (
-    <div className="w-full min-h-[50vh] p-4 flex items-center justify-center relative">
+    <div className="w-full min-h-[45vh] pl-4 pr-4 flex items-center justify-center relative border-[1px] border-gray-800 rounded-md">
       <div className="w-[80%] min-h-[30vh] flex items-center justify-center border-4 border-green-700 rounded-lg relative">
         <div className="w-[30px] h-[30px] flex items-center justify-center  rounded-full border-4 border-red-600 bg-black absolute -top-4 -left-4">
           {data[0] < 5 ? (
@@ -20,7 +20,7 @@ export default function IITKMap({ data }: dataProps) {
             </p>
           )}
         </div>
-        <div className="absolute pl-2 pr-2 top-4 -left-8 backdrop-blur-sm rounded-lg border-[1px] border-slate-300">
+        <div className="w-[130px] absolute pl-2 pr-2 top-4 -left-8 backdrop-blur-sm rounded-lg border-[1px] border-slate-300">
           <p>Sensor 1</p>
           <p>PK Kelkar Library</p>
         </div>
@@ -36,7 +36,7 @@ export default function IITKMap({ data }: dataProps) {
             </p>
           )}
         </div>
-        <div className="absolute pl-2 pr-2 top-4 -right-8 backdrop-blur-sm rounded-lg border-[1px] border-slate-300">
+        <div className="w-[130px] absolute pl-2 pr-2 top-4 -right-8 backdrop-blur-sm rounded-lg border-[1px] border-slate-300">
           <p>Sensor 2</p>
           <p>Old SAC Complex</p>
         </div>
@@ -51,7 +51,7 @@ export default function IITKMap({ data }: dataProps) {
             </p>
           )}
         </div>
-        <div className="absolute pl-2 pr-2 bottom-12 -left-8 backdrop-blur-sm rounded-lg border-[1px] border-slate-300">
+        <div className="w-[130px] absolute pl-2 pr-2 bottom-12 -left-8 backdrop-blur-sm rounded-lg border-[1px] border-slate-300">
           <p>Sensor 3</p>
           <p>New Core Labs</p>
         </div>
@@ -67,7 +67,7 @@ export default function IITKMap({ data }: dataProps) {
             </p>
           )}
         </div>
-        <div className="absolute pl-2 pr-2 bottom-12 -right-8 backdrop-blur-sm rounded-lg border-[1px] border-slate-300">
+        <div className=" absolute pl-2 pr-2 bottom-12 -right-8 backdrop-blur-sm rounded-lg border-[1px] border-slate-300">
           <p>Sensor 4</p>
           <p>Flight Laboratory</p>
         </div>
